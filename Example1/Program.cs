de63f91efd4e4ed3df97d3218ddc7007cda17551
Antonkina Anastaiya <floweri888@mail.ru>
2022-11-30 00:05:29 +0300

    Add example1

---
+++ Example1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine ("Введите число А");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите число B");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int numberMax = 0;
+if (numberA > numberB) {
+    numberMax = numberA;
+}
+else {
+    numberMax = numberB;
+}
+Console.Write ("Максимальное число ");
+Console.WriteLine (numberMax);
